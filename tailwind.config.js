b7970a8dc030dcb8e0c94b64b73e85037de9f1c0
josptrra <118672255+josptrra@users.noreply.github.com>
2024-03-05 21:23:39 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", "sans-serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        "main-image": "url('/images/hero.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
